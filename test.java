667d7dd8041cb014c43b4a90ab44e403a493b85c
takarahayato <e185734@ie.u-ryukyu.ac.jp>
2020-09-08 23:51:19 +0900

    java file fix

--- test.java
+++ test.java
@@ -1,1 +1,11 @@
-sistem.out.println("holle world");
+
+class Sample {
+  public static void main(String args[]) {
+    System.out.println(2+3);
+
+    String str1 = "Hello ";
+    String str2 = "World!";
+    System.out.print(str1);
+    System.out.println(str2);
+  }
+}
